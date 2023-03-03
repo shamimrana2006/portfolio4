8f1d8e87e5f3d29f927460c0241599cd253f42c0
mdshamimrana20 <mdshamimrana20@gmail.com>
2023-03-03 13:03:07 +0600

    responsive and currection

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var togglebut = document.querySelector(".menuBttn");
+var nabmenu = document.querySelector(".nav-menu");
+
+togglebut.addEventListener("click", () => {
+    nabmenu.classList.toggle("Navactive");
+});
+
